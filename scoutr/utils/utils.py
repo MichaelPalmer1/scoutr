86e81a178bd1359182e1478f8913a886c0294e2e
Michael Palmer <MichaelPalmer1@users.noreply.github.com>
2020-11-10 13:41:44 -0500

    that's not needed

--- scoutr/utils/utils.py
+++ scoutr/utils/utils.py
@@ -1,10 +1,3 @@
-try:
-    import sentry_sdk
-    has_sentry = True
-except ImportError:
-    has_sentry = False
-
-
 def merge_lists(primary_list: list, secondary_list: list) -> list:
     """Merge two lists together and return a unique list"""
     return primary_list + [item for item in secondary_list if item not in primary_list]
